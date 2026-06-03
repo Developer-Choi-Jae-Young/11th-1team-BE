4fc45bd44018e2a43c4b7b8da33429a8272bc5f7
최재영 <cjy951213@naver.com>
2026-06-03 20:44:28 +0900

    Refactor: 회원 탈퇴 및 소셜 연동 해제 로직 고도화

--- src/main/java/org/example/knockin/global/auth/service/AbstractOauth2DeleteService.java
+++ src/main/java/org/example/knockin/global/auth/service/AbstractOauth2DeleteService.java
@@ -3,19 +3,15 @@ package org.example.knockin.global.auth.service;
 import org.example.knockin.entity.auth.LoginProviderType;
 import org.springframework.web.client.RestTemplate;
 
-public class AbstractOauth2DeleteService implements Oauth2DeleteService{
+public abstract class AbstractOauth2DeleteService implements Oauth2DeleteService{
     protected final RestTemplate restTemplate;
 
-    public AbstractOauth2DeleteService(RestTemplate restTemplate) {
+    protected AbstractOauth2DeleteService(RestTemplate restTemplate) {
         this.restTemplate = restTemplate;
     }
 
     @Override
-    public boolean requestUnlink(String providerId) {
-        return false;
-    }
+    public abstract boolean requestUnlink(String providerId);
 
-    public LoginProviderType getProviderType() {
-        return null;
-    }
+    public abstract LoginProviderType getProviderType();
 }
