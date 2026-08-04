a10178ffaefe0e6c19d7b84229aa8088ed4a62b2
yourjin <155814455+yourjinKR@users.noreply.github.com>
2026-08-04 22:47:02 +0900

    fix: 별칭을 매핑

--- src/main/java/org/example/knockin/repository/board/impl/RoommateBoardRepositoryImpl.java
+++ src/main/java/org/example/knockin/repository/board/impl/RoommateBoardRepositoryImpl.java
@@ -104,8 +104,8 @@ public class RoommateBoardRepositoryImpl implements RoommateBoardRepositoryCusto
                 .leftJoin(boardRegion.parent, parentRegion)
                 .leftJoin(parentRegion.parent, grandParentRegion)
                 .join(roommateBoard.member, member)
-                .leftJoin(basicInformation).on(basicInformation.member.eq(member))
-                .leftJoin(basicInformation).on(latestBasicInformationIdEq(latestBasicInformation))
+                .leftJoin(latestBasicInformation).on(latestBasicInformation.member.eq(member))
+                .leftJoin(latestBasicInformation).on(latestBasicInformationIdEq(latestBasicInformation))
                 //.leftJoin(member.basicInformations, latestBasicInformation).on(latestBasicInformationIdEq(latestBasicInformation))
                 .leftJoin(basicInformationFile)
                 .on(basicInformationFile.id.eq(
@@ -292,8 +292,8 @@ public class RoommateBoardRepositoryImpl implements RoommateBoardRepositoryCusto
                 .leftJoin(boardRegion.parent, parentRegion)
                 .leftJoin(parentRegion.parent, grandParentRegion)
                 .join(roommateBoard.member, member)
-                .leftJoin(basicInformation).on(basicInformation.member.eq(member))
-                .leftJoin(basicInformation).on(latestBasicInformationIdEq(latestBasicInformation))
+                .leftJoin(latestBasicInformation).on(latestBasicInformation.member.eq(member))
+                .leftJoin(latestBasicInformation).on(latestBasicInformationIdEq(latestBasicInformation))
                 //.leftJoin(member.basicInformations, latestBasicInformation).on(latestBasicInformationIdEq(latestBasicInformation))
                 .leftJoin(basicInformationFile)
                 .on(basicInformationFile.id.eq(
